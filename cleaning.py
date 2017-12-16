3c17cb3e898f950fa557b9c2487794ed3e81e834
IrisGuo <yanfguo@vub.ac.be>
2017-12-16 10:11:02 +0100

    add first step

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,42 @@
 # author yanfang.guo@vub.be Dec.16th 2017
+import nltk
 
 from csv_helper import *
 
 tweets =  CSVHelper.load_csv("./Dataset/Tweets_2016London.csv")
 
+# print out some examples
+tweets_token = []
+for i in range(3):
+    print(tweets[i])
+
+# 1.Tokenization
+tknzr = nltk.TweetTokenizer(strip_handles=True, reduce_len=True)
+for i in  range(len(tweets)):
+    tweets_token.append(tknzr.tokenize(tweets[i]))
+
+# print out some examples
+for i in range(3):
+    print(tweets_token[i])
+
+
+# 2. Stop-word removal
+#download the nltk stopwords
+#nltk.download('stopwords')
+stop_words_eng = nltk.corpus.stopwords.words('english')
+tweets_del_stword = []
+for i in tweets_token:
+    temp = [w for w in i if w.lower() not in stop_words_eng]
+    tweets_del_stword.append(temp)
+
+'''
+# testfunction to see if it works
+
+length = []
+for i in range(len(tweets)):
+    length.append([len(tweets_token[i]),len(tweets_del_stword[i])])
+
+print(length)
+'''
+
+# 3. stemming
